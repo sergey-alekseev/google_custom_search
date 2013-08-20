25bc44291c943f6bf3984ce0ce3f9d979023987d
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2013-08-21 02:39:27 +0300

    update log info to be more honest

--- run.rb
+++ run.rb
@@ -8,7 +8,7 @@ begin
   while queries_count <= 100
     state = State.next_state
     queries_count += ManageBuildingParser.contact_infos_for(state.code)
-    LOGGER.info "#{queries_count} queries performed for #{state.code}"
+    LOGGER.info "#{queries_count} queries have been already performed. Last state: #{state.code}."
     state.update_attributes(searched_by: true)
   end
 rescue
