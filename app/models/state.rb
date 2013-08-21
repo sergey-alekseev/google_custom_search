e2f37bd5fa85cf6e117ea53a73b70505a0dda0f1
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2013-08-21 17:19:41 +0300

    start afresh when all states have been fetched

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,8 @@
 require 'active_record'
 class State < ActiveRecord::Base
   def self.next_state
-    State.where(searched_by: false).first
+    State.where(searched_by: false).first || (
+      State.find_each { |s| s.update_attributes(searched_by: false) }
+      State.first)
   end
 end
