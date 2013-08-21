aff4d53bb46b19dd0d5933745bc4dac872a09c2e
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2013-08-21 17:08:32 +0300

    run db import to csv after 20 minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,9 @@ set :output, "#{project_root}/log/cron_log.log"
 
 every 1.day, at: '1:50 pm' do
   command "cd #{project_root} && ruby run.rb"
+end
+
+every 1.day, at: '2:10 pm' do
   command "cd #{project_root} && " \
     "sqlite3 #{project_root}/db/contact_infos.sqlite3 < #{project_root}/db/import_to_csv.sql"
 end
