515e5f2abbe7b9d58c1e869d76573cdeb2d87cef
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2013-09-02 14:44:31 +0300

    refactor to be 'keyword'

--- run.rb
+++ run.rb
@@ -8,9 +8,10 @@ begin
   queries_count = 0
   while queries_count <= 100
     state = State.next_state
-    queries_count += Parsers::ManageBuilding.contact_infos_for(state.code)
-    queries_count += Parsers::AppFolio.contact_infos_for(state.code)
-    LOGGER.info "#{queries_count} queries have been already performed. Last state: #{state.code}."
+    keyword = state.code
+    queries_count += Parsers::ManageBuilding.contact_infos_for(keyword)
+    queries_count += Parsers::AppFolio.contact_infos_for(keyword)
+    LOGGER.info "#{queries_count} queries have been already performed. Last keyword: #{keyword}."
     state.update_attributes(searched_by: true)
   end
 rescue
